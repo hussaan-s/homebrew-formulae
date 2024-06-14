eb42333c65ed0bdbb6d1566f17516c9e4cd12ce3
Muhammad Hussaan Saeed <hussaan.s@gmail.com>
2024-06-14 16:11:27 -0400

    Update xcresultparser.rb

--- xcresultparser.rb
+++ xcresultparser.rb
@@ -2,7 +2,7 @@ class Xcresultparser < Formula
   desc "Parse .xcresult files and print summary in different formats"
   homepage "https://github.com/a7ex/xcresultparser"
   version "1.6.2"
-  url "https://github.com/a7ex/xcresultparser/archive/1.6.2.tar.gz"
+  url "https://github.com/hussaan-s/xcresultparser/archive/1.6.2.tar.gz"
   sha256 "936673bbbab678b65cb8c2e805ac2c520e4db39ad95342c3c833dc2bbb442c42"
   license "MIT"
 
